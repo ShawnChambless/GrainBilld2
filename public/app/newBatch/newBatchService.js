e80cc11886765f234d6321a8b9ee392e1772f5cd
ShawnChambless <shawn.chambless@gmail.com>
2015-10-10 16:40:09 -0600

    adding grain functionality is working.

--- public/app/newBatch/newBatchService.js
+++ public/app/newBatch/newBatchService.js
@@ -1,89 +1,62 @@
 angular.module('GrainBilld')
 .service('newBatchService', function() {
-    var grainInRecipe  = [];
-    var hopsInRecipe   = [];
-    var yeastInRecipe  = [];
+    var recipeStats   = [{og: 0}];
+    this.grainInRecipe = [];
+    this.hopsInRecipe  = [];
+    this.yeastInRecipe = [];
 
 
     this.addIngredient = function(ingredientType, ingredient) {
-        switch(ingredientType) {
-            case 'grain':
-                addIngredientToRecipe(ingredient, addGrain);
-                break;
-            case 'hops':
-                addIngredientToRecipe(ingredient, addHops);
-                break;
-            case 'yeast':
-                addIngredientToRecipe(ingredient, addYeast);
-                break;
-        }
+        addIngredient(ingredientType, ingredient);
+        console.log(this.grainInRecipe);
     };
 
-    function addIngredientToRecipe(ingredient, cb) {
-        cb(ingredient);
+    var addGrainToRecipe = function(grain, calcOg, calcSRM) {
+        this.grainInRecipe.push({grain: grain, amount: 10, batchSize: 10});
+        var OG = calcOg(grain);
+        var SRM = calcSRM(grain);
+        return OG(), SRM(), this.grainInRecipe;
+    }.bind(this);
+
+    var calculateOGOfRecipe = function(grain) {
+        var og = 1.000, gpOfGrain = 0, itemToAdd = 0, batchSize = 10, amount = 10, sg = parseFloat(grain.sg);
+        return function() {
+            if (!itemToAdd) {
+                itemToAdd = ((sg - 1)*1000 * amount);
+                gpOfGrain = itemToAdd;
+            } else {
+                gpOfGrain = (itemToAdd + ((sg - 1)*1000) * amount);
+                itemToAdd = gpOfGrain;
+            }
+            og = ((gpOfGrain * 0.75 / batchSize)/1000 + 1).toFixed(3);
+            recipeStats.find(function(ele) { ele.og += parseFloat(og); });
+            console.log(recipeStats[0].og);
+            return og;
+        };
+    }.bind(this);
+
+    var calculateSRMOfRecipe = function(grain) {
+        var recipeSrm = 0, MCU = 0, amount = 10, batchSize = 10, lovibond = parseFloat(grain.lovibond);
+        return function() {
+            MCU = ((lovibond * amount)/batchSize);
+            recipeSrm += (1.4922*(Math.pow(MCU, 0.6859))).toFixed(1);
+            return recipeSrm;
+        };
+
+    };
+
+    function addIngredient(ingredientType, ingredient) {
+        switch(ingredientType) {
+            case 'grain':
+                addGrainToRecipe(ingredient, calculateOGOfRecipe, calculateSRMOfRecipe);
+                break;
+            case 'hops':
+                addHopsToRecipe(ingredient);
+                break;
+            case 'yeast':
+                addYeastToRecipe(ingredient);
+                break;
+        }
     }
-    function addGrain(grain, amount, batchSize) {
-        console.log('addGrain', grain);
-        this.itemToAdd = 0;
-        this.gpOfGrain = 0;
-        this.OG = 1.000;
-        amount = 0;
-        grainInRecipe.push({grain: grain, amount: amount, batchSize: batchSize});
-        if (!itemToAdd) {
-            this.itemToAdd = ((grain.sg - 1)*1000 * amount);
-            this.gpOfGrain = this.itemToAdd;
-        } else {
-            this.gpOfGrain = (this.itemToAdd + ((parseFloat(grain.sg) - 1)*1000) * amount);
-            this.itemToAdd = this.gpOfGrain;
-        }
-        this.MCU = (parseFloat((grain.lovibond * amount)/batchSize));
-        this.recipeSrm = (1.4922*(Math.pow(MCU, 0.6859))).toFixed(1);
-        this.OG = ((gpOfGrain * 0.75 / batchSize)/1000 + 1).toFixed(3);
-    } addGrain.bind(this);
-
-    function addHops(hops, amount, boilTime) {
-        console.log('addHops', hops);
-        this.IBUOfGrain = 0;
-        this.hopsItemToAdd = 0;
-        this.hopsInRecipe.push({hops: hops, amount: amount, boilTime: boilTime});
-        this.boilTime.push(boilTime);
-        this.hopUtilization = 0;
-        if(boilTime === 0) {
-            this.hopUtilization = 0;
-        } else if (boilTime > 0 && boilTime <= 9) {
-            this.hopUtilization = 0.05;
-        } else if (boilTime > 9 && boilTime <= 19) {
-            this.hopUtilization = 0.12;
-        } else if (boilTime > 19 && boilTime <= 29) {
-            this.hopUtilization = 0.15;
-        } else if (boilTime > 29 && boilTime <= 44) {
-            this.hopUtilization = 0.19;
-        } else if (boilTime > 44 && boilTime <= 59) {
-            this.hopUtilization = 0.22;
-        } else if (boilTime > 59 && boilTime <= 74) {
-            this.hopUtilization = 0.24;
-        } else if (boilTime > 74) {
-            this.hopUtilization = 0.27;
-        }
-        this.AAU = amount * (hops.alphaAcid);
-        if (!hopsItemToAdd) {
-            this.IBUOfGrain = this.AAU * hopUtilization * 74.89 / this.batchSize;
-            hopsItemToAdd = IBUOfGrain;
-            this.IBU = IBUOfGrain.toFixed(1);
-        }
-        else {
-            this.IBUOfGrain = (this.AAU * hopUtilization * 74.89 / this.batchSize) + hopsItemToAdd;
-            hopsItemToAdd = IBUOfGrain;
-            this.IBU = IBUOfGrain.toFixed(1);
-        }
-    } addHops.bind(this);
-
-    function addYeast(yeast, amount) {
-        console.log('addYeast', yeast);
-        this.yeastInRecipe.push({yeast: yeast, amount: amount});
-        thisattenuation = (this.yeastInRecipe.maximumAttenuation + this.yeastInRecipe.minimumAttenuation)/2;
-        this.FG = (1+(this.OG / (attenuation))).toFixed(3);
-        this.ABV = ((76.08) * (this.OG - this.FG) / (1.775 - this.OG) * (this.FG / 0.794)).toFixed(2);
-    } addYeast.bind(this);
 
 });
