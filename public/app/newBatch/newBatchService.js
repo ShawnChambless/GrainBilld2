5f372b03496d471362ca3b001ab9371fdd15b83f
ShawnChambless <shawn.chambless@gmail.com>
2015-10-10 22:22:30 -0600

    adding hops and yeast functionality now works

--- public/app/newBatch/newBatchService.js
+++ public/app/newBatch/newBatchService.js
@@ -1,10 +1,9 @@
 angular.module('GrainBilld')
 .service('newBatchService', function() {
-    var recipeStats   = [{og: 0}];
+    var recipeStats   = [{og: 0}, {ibu: 0}, {abv: 0}];
     this.grainInRecipe = [];
     this.hopsInRecipe  = [];
     this.yeastInRecipe = [];
-
 
     this.addIngredient = function(ingredientType, ingredient) {
         addIngredient(ingredientType, ingredient);
@@ -15,7 +14,7 @@ angular.module('GrainBilld')
         this.grainInRecipe.push({grain: grain, amount: 10, batchSize: 10});
         var OG = calcOg(grain);
         var SRM = calcSRM(grain);
-        return OG(), SRM(), this.grainInRecipe;
+        return OG(), SRM();
     }.bind(this);
 
     var calculateOGOfRecipe = function(grain) {
@@ -30,7 +29,6 @@ angular.module('GrainBilld')
             }
             og = ((gpOfGrain * 0.75 / batchSize)/1000 + 1).toFixed(3);
             recipeStats.find(function(ele) { ele.og += parseFloat(og); });
-            console.log(recipeStats[0].og);
             return og;
         };
     }.bind(this);
@@ -42,8 +40,71 @@ angular.module('GrainBilld')
             recipeSrm += (1.4922*(Math.pow(MCU, 0.6859))).toFixed(1);
             return recipeSrm;
         };
+    };
+
+    var addHopsToRecipe = function(hops, amount, batchSize, boilTime, calculateIBUOfRecipe, findHopUtilization) {
+        this.hopsInRecipe.push({hops: hops, amount: amount, boilTime: boilTime});
+        var util = findHopUtilization(boilTime);
+        var IBU = calculateIBUOfRecipe(hops, amount, batchSize, util);
+        return IBU(), util();
+    }.bind(this);
+
+    var findHopUtilization = function(boilTime) {
+        boilTime = 10;
+        var hopUtilization = 0;
+        return function() {
+            if(boilTime === 0) {
+                hopUtilization = 0;
+            } else if (boilTime > 0 && boilTime <= 9) {
+                hopUtilization = 0.05;
+            } else if (boilTime > 9 && boilTime <= 19) {
+                hopUtilization = 0.12;
+            } else if (boilTime > 19 && boilTime <= 29) {
+                hopUtilization = 0.15;
+            } else if (boilTime > 29 && boilTime <= 44) {
+                hopUtilization = 0.19;
+            } else if (boilTime > 44 && boilTime <= 59) {
+                hopUtilization = 0.22;
+            } else if (boilTime > 59 && boilTime <= 74) {
+                hopUtilization = 0.24;
+            } else if (boilTime > 74) {
+                hopUtilization = 0.27;
+            }
+            return hopUtilization;
+        };
 
     };
+
+    var calculateIBUOfRecipe = function(hops, amount, batchSize, utilization) {
+        var hopUtilization = utilization(),
+        IBUOfHops     = 0,
+        hopsItemToAdd = 0;
+        AAU           = amount * (hops.alphaAcid);
+        return function() {
+            if (!hopsItemToAdd) {
+                IBUOfHops = AAU * hopUtilization * 74.89 / batchSize;
+                hopsItemToAdd = IBUOfHops;
+                IBU = IBUOfHops.toFixed(1);
+            } else {
+                IBUOfHops = (AAU * hopUtilization * 74.89 / batchSize) + hopsItemToAdd;
+                hopsItemToAdd = IBUOfHops;
+                IBU = IBUOfHops.toFixed(1);
+            }
+            recipeStats.find(function(ele) { ele.ibu += parseFloat(IBU); });
+            return IBU;
+        };
+
+    };
+
+    var addYeastToRecipe = function(yeast) {
+        this.yeastInRecipe.push({yeast: yeast});
+        var attenuation = 0;
+        attenuation = ((yeast.maximumAttenuation + yeast.minimumAttenuation)/2);
+        var OG = recipeStats[0].og;
+        var FG = (1+(OG / (attenuation))).toFixed(3);
+        var ABV = ((76.08) * (OG - FG) / (1.775 - OG) * (FG / 0.794)).toFixed(2);
+        return parseFloat(FG), parseFloat(ABV);
+    }.bind(this);
 
     function addIngredient(ingredientType, ingredient) {
         switch(ingredientType) {
@@ -51,7 +112,10 @@ angular.module('GrainBilld')
                 addGrainToRecipe(ingredient, calculateOGOfRecipe, calculateSRMOfRecipe);
                 break;
             case 'hops':
-                addHopsToRecipe(ingredient);
+                amount = 10;
+                batchSize = 10;
+                boilTime = 10;
+                addHopsToRecipe(ingredient, amount, batchSize, boilTime, calculateIBUOfRecipe, findHopUtilization);
                 break;
             case 'yeast':
                 addYeastToRecipe(ingredient);
