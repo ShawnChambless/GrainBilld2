6d0ecf6e4fad3c300959c2ddb1b209d95c4d26bf
ShawnChambless <shawn.chambless@gmail.com>
2016-01-08 12:35:56 -0500

    add headers

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var express     = require( 'express' ),
     app         = express(),
     httpServer  = http.createServer(app),
     allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', 'http://grainbilld.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
       next();
